d4ebd5603aa9ee91d2106732d98ef1bfe71acc74
Nik Sokolov <frostys@list.ru>
2020-02-20 10:23:48 +0300

    Initial commit.

---
+++ Main.java
@@ -0,0 +1,11 @@
+package ru.another_finch.jframeExample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleGui app = new SimpleGui();
+        app.setVisible(true);
+
+    }
+
+}
